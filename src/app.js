6328b8aea0af17ecd35b16cf71d092379ca0f23e
Raviganesh M <raviganeshmbhat999@gmail.com>
2023-11-26 22:02:14 +0530

    user api end points (WIP)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import userRouter from "./routes/user.routes.js";
 
 export const app = express();
 
@@ -12,7 +13,9 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
